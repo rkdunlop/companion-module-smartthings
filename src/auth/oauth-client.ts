338ca9a7c5ea7671308fc4d86ec4c20db7974fd0
R. Kyle Dunlop <rkdunlop@users.noreply.github.com>
2026-07-30 17:58:56 -0400

    feat: wire OAuth token provider to OAuth client

--- src/auth/oauth-client.ts
+++ src/auth/oauth-client.ts
@@ -5,7 +5,21 @@ export interface OAuthTokens {
 }
 
 export class OAuthClient {
-	public buildAuthorizationUrl(): string {}
+	public constructor(
+		private readonly clientId: string,
+		//private readonly _clientSecret: string,
+		private readonly redirectUri: string,
+	) {}
+
+	public buildAuthorizationUrl(): string {
+		const url = new URL('https://api.smarthings.com/oauth/authorize')
+
+		url.searchParams.set('client_id', this.clientId)
+		url.searchParams.set('redirect_uri', this.redirectUri)
+		url.searchParams.set('response_type', 'code')
+
+		return url.toString()
+	}
 
 	public async exchangeAuthorizationCode(_code: string): Promise<OAuthTokens> {
 		throw new Error('Not Implemented')
--- src/auth/oauth-token-provider.ts
+++ src/auth/oauth-token-provider.ts
@@ -1,11 +1,32 @@
 import type { TokenProvider } from './token-provider.js'
+import { OAuthClient, type OAuthTokens } from './oauth-client.js'
 
 export class OAuthTokenProvider implements TokenProvider {
 	private accessToken?: string
 	private refreshToken?: string
 	private expiresAt = 0
 
+	public constructor(
+		private readonly oauthclient: OAuthClient,
+		tokens?: OAuthTokens,
+	) {
+		this.accessToken = tokens?.accessToken
+		this.refreshToken = tokens?.refreshToken
+		this.expiresAt = tokens?.expiresAt ?? 0
+	}
+
 	public async getAccessToken(): Promise<string> {
-		throw new Error('OAuth authentication has not been completed.')
+		if (!this.accessToken || !this.refreshToken) {
+			throw Error('NO ACCESS TOKEN')
+		}
+
+		if (this.expiresAt <= Date.now()) {
+			const tokens = await this.oauthclient.refreshAccessToken(this.refreshToken)
+
+			this.accessToken = tokens.accessToken
+			this.refreshToken = tokens.refreshToken
+			this.expiresAt = tokens.expiresAt
+		}
+		return this.accessToken
 	}
 }
